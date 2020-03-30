4b5c69daeb2fe41eea1130f1a86faf959adab423
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-30 22:19:36 +0545

    Airbnb style guide

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const pauseButton = document.querySelector("#pause");
 const timerInput = document.querySelector("input");
 
 const circle = document.querySelector("circle");
-const perimeter = 2 * circle.getAttribute("r") * Math.PI; 
+const perimeter = 2 * circle.getAttribute("r") * Math.PI;
 circle.setAttribute("stroke-dasharray", perimeter);
 
 let offset = 0
 let part = []
 
 const stopwatchObj = new Stopwatch(resumeButton, pauseButton, timerInput, {
-    timerStarted(totalTIme){
+    timerStarted(totalTIme) {
         console.log("Timer Started")
         part.push(totalTIme)
     },
@@ -19,14 +19,14 @@ const stopwatchObj = new Stopwatch(resumeButton, pauseButton, timerInput, {
     },
     timerTicked() {
         console.log("Timer ticked down by one second", part[0])
-        offset -= perimeter/(part[0] / 0.01)
-        circle.setAttribute("stroke-dashoffset", `${offset}`); 
+        offset -= perimeter / (part[0] / 0.01)
+        circle.setAttribute("stroke-dashoffset", `${offset}`);
     },
     timerComplete() {
         timerInput.value = ""
-        circle.setAttribute("stroke-dashoffset", "0"); 
+        circle.setAttribute("stroke-dashoffset", "0");
         part = [];
         console.log("Timer Completed");
     }
-    
+
 });
