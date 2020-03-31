5af35585904cc60f49579b317bff463a5529dc58
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-31 13:17:37 +0545

    Improved

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const resumeButton = document.querySelector("#resume");
-const pauseButton = document.querySelector("#pause");
-const timerInput = document.querySelector("input");
+const resumeButton = document.querySelector('#resume')
+const pauseButton = document.querySelector('#pause')
+const timerInput = document.querySelector('input')
 
-const circle = document.querySelector("circle");
-const perimeter = 2 * circle.getAttribute("r") * Math.PI;
-circle.setAttribute("stroke-dasharray", perimeter);
+const circle = document.querySelector('circle')
+const perimeter = 2 * circle.getAttribute('r') * Math.PI
+circle.setAttribute('stroke-dasharray', perimeter)
+
+const name = 'asas'
 
 let offset = 0
 let part = []
 
 const stopwatchObj = new Stopwatch(resumeButton, pauseButton, timerInput, {
-    timerStarted(totalTIme) {
-        console.log("Timer Started")
-        part.push(totalTIme)
-    },
-    timerPaused() {
-        console.log("Timer Paused")
-    },
-    timerTicked() {
-        console.log("Timer ticked down by one second", part[0])
-        offset -= perimeter / (part[0] / 0.01)
-        circle.setAttribute("stroke-dashoffset", `${offset}`);
-    },
-    timerComplete() {
-        timerInput.value = ""
-        circle.setAttribute("stroke-dashoffset", "0");
-        part = [];
-        console.log("Timer Completed");
-    }
-
-});
+  timerStarted(totalTIme) {
+    console.log('Timer Started')
+    part.push(totalTIme)
+  },
+  timerPaused() {
+    console.log('Timer Paused')
+  },
+  timerTicked() {
+    console.log('Timer ticked down by one second', part[0])
+    offset -= perimeter / (part[0] / 0.01)
+    circle.setAttribute('stroke-dashoffset', `${offset}`)
+  },
+  timerComplete() {
+    timerInput.value = ''
+    circle.setAttribute('stroke-dashoffset', '0')
+    part = []
+    console.log('Timer Completed')
+  },
+})
--- stopwatch.js
+++ stopwatch.js
@@ -1,65 +1,60 @@
-class Stopwatch{
-    constructor(resumeButton, pauseButton, timerInput, callbacks){
-        this.resumeButton = resumeButton;
-        this.pauseButton = pauseButton;
-        this.timerInput = timerInput;
-        
-        this.resumeButton.addEventListener("click", this.start);
-        this.timerInput.addEventListener("keydown", (evt) => {
-            if (evt.code == "Enter"){
-                this.start()
-            }
-        })
-        this.pauseButton.addEventListener("click", this.pause);
+class Stopwatch {
+  constructor(resumeButton, pauseButton, timerInput, callbacks) {
+    this.resumeButton = resumeButton
+    this.pauseButton = pauseButton
+    this.timerInput = timerInput
 
-        
+    this.resumeButton.addEventListener('click', this.start)
+    this.timerInput.addEventListener('keydown', evt => {
+      if (evt.code == 'Enter') {
+        this.start()
+      }
+    })
+    this.pauseButton.addEventListener('click', this.pause)
 
-        if (callbacks) {
-            this.timerStarted = callbacks.timerStarted;
-            this.timerTicked = callbacks.timerTicked;
-            this.timerComplete = callbacks.timerComplete;
-            this.timerPaused = callbacks.timerPaused
-        }
-
+    if (callbacks) {
+      this.timerStarted = callbacks.timerStarted
+      this.timerTicked = callbacks.timerTicked
+      this.timerComplete = callbacks.timerComplete
+      this.timerPaused = callbacks.timerPaused
     }
-    
-    start = () => {
-        if (this.timerStarted) {
-            this.timerStarted(this.timerInput.value)
-        };
-        this.timerInterval = setInterval(this.tick, 10);
-        
+  }
+
+  start = () => {
+    if (this.timerStarted) {
+      this.timerStarted(this.timerInput.value)
     }
-    
-    pause = () => {
-        if (this.timeLeft !== 0){
+    this.timerInterval = setInterval(this.tick, 10)
+  }
 
-            if (this.timerPaused){
-                this.timerPaused()
-            }
-        }
-        clearInterval(this.timerInterval);
+  pause = () => {
+    if (this.timeLeft !== 0) {
+      if (this.timerPaused) {
+        this.timerPaused()
+      }
     }
-    
-    tick = () => {
-        if (this.timerInput.value > 0) {
-            this.timeLeft = this.timeLeft - 0.01;
-            if (this.timerTicked) {
-                this.timerTicked()
-            };
-            return console.log(this.timerInput.value);
-        } 
-        this.pause()
-        if (this.timerComplete) {
-            this.timerComplete()
-        };     
-    };
+    clearInterval(this.timerInterval)
+  }
 
-    get timeLeft() {
-        return parseFloat(this.timerInput.value);
-    };
+  tick = () => {
+    if (this.timerInput.value > 0) {
+      this.timeLeft = this.timeLeft - 0.01
+      if (this.timerTicked) {
+        this.timerTicked()
+      }
+      return console.log(this.timerInput.value)
+    }
+    this.pause()
+    if (this.timerComplete) {
+      this.timerComplete()
+    }
+  }
 
-    set timeLeft(time) {
-        this.timerInput.value = time.toFixed(2);
-    };
+  get timeLeft() {
+    return parseFloat(this.timerInput.value)
+  }
+
+  set timeLeft(time) {
+    this.timerInput.value = time.toFixed(2)
+  }
 }
